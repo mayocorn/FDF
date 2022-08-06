dbf0bbc0a0ab4699f17d26ece682c535e494e027
mayocorn <happy0926_sk@yahoo.co.jp>
2022-08-06 16:40:19 +0900

    feat(exit*): exit系関数をひとつのファイルにまとめた

---
+++ include/utils.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/30 12:59:34 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/08/06 16:39:30 by mayocorn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+void	exit_caused_by_invalid_arguments(void);
+void	exit_caused_by_invalid_map(void);
+void	exit_caused_by_system_error(void);
+
+#endif
---
+++ src/utils/exit_fdf.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_fdf.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/06 16:33:40 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/08/06 16:38:34 by mayocorn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+void	exit_caused_by_invalid_arguments(void)
+{
+	ft_putstr_fd("FDF : invalid arguments\n", STDERR_FILENO);
+	ft_putstr_fd("Usage : ./fdf <filename>\n", STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
+
+void	exit_caused_by_invalid_map(void)
+{
+	ft_putstr_fd("FDF : invalid map\n", STDERR_FILENO);
+	ft_putstr_fd("map format : <altitude>, <color> ...\n", STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
+
+void	exit_caused_by_system_error(void)
+{
+	perror("FDF");
+	exit(EXIT_FAILURE);
+}
