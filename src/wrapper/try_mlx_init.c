6e80eb83f0d23d6985349e5dd95c9ee9757e0806
mayocorn <happy0926_sk@yahoo.co.jp>
2022-08-08 18:59:20 +0900

    feat(try_mlx_init): ファイルを追加

--- src/wrapper/try_mlx_init.c
+++ src/wrapper/try_mlx_init.c
@@ -1,21 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   wrapper.h                                          :+:      :+:    :+:   */
+/*   try_mlx_init.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/30 13:55:00 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/08/08 18:58:50 by mayocorn         ###   ########.fr       */
+/*   Created: 2022/08/08 18:53:57 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/08/08 18:58:13 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WRAPPER_H
-# define WRAPPER_H
+#include "mlx.h"
+#include "utils.h"
 
-void		*try_malloc(size_t size);
-void		*try_mlx_init(void);
-int			try_open(const char *path, int oflag);
-long long	try_strtoll(const char *str, int base);
+void	*try_mlx_init(void)
+{
+	void	*mlx;
 
-#endif
+	mlx = mlx_init();
+	if (mlx == NULL)
+		exit_caused_by_mlx();
+	return (mlx);
+}
